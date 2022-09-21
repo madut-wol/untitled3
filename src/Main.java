2cbccc58f70216d207a229c4351c4d4d264cd0ab
Makol Ariik <mawelbukringthii@gmail.com>
2022-09-21 14:13:15 +0300

    added  int a = 100;        int b = 120;        int c = a * b;

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int a = 100;
+        int b = 120;
+        int c = a * b;
+
+        System.out.println("The Final Answer is: " + c);
     }
 }
